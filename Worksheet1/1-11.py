b821b9392322bec8f5c2120f30af33a1e986cce0
Rahul0506 <rahul.gidijala@gmail.com>
2017-03-03 13:34:13 +0800

    Add files via upload

---
+++ Worksheet1/1-11.py
@@ -0,0 +1,93 @@
+1.	12
+
+2.	i.		Not legal, reserved
+	ii. 	Legal
+	iii.	Legal
+	iv. 	Legal
+	v. 		Not legal, starts with a number
+	vi.		Not legal, special character
+	vii. 	Legal
+	viii.	Legal
+
+3.	i.		bool
+	ii.		float
+	iii.	string
+	iv.		int
+	v.		string
+	vi.		float
+	vii.	string
+	viii.	bool
+	ix.		string
+	x.		int
+	xi.		float
+	xii.	bool
+	xiii.	string
+	
+4.	i.		False
+	ii.		True
+	iii.	True
+	iv.		True
+	v.		False
+
+5.	i.		True
+	ii. 	0.5
+	iii.	0.5
+	iv. 	False
+	v. 		False
+	vi.		False
+	vii. 	53True
+	viii.	10,20
+	
+6.	i.		none
+	ii.		15
+	iii.	14
+	iv.		22
+	v.		none
+
+7.	i.		Infinte Loop
+	ii.		X X X X
+	iii. 	X O O
+
+8.	i.		10
+			9
+			8
+			7
+			6
+			5
+			4
+	
+	ii.		Error, i is not defined
+	iii.	10
+			9
+			8
+			7
+
+9.	i.		SyntaxError
+	ii.		O
+			O
+			O
+			
+	iii.	O
+			O
+			O
+	
+	iv.		O
+			O
+			O
+			O
+			
+10.	i.		XO XO XO
+	ii.		Infinite Loop
+	iii.	IndentationError
+	
+11.	
+	a = 1
+	while a < 3:
+		b = 1
+		while b < 3:
+			if a == 2:
+				print("X", end="")
+			print("O", end="")
+			b += 1
+		print("O", end="")
+		a += 1
