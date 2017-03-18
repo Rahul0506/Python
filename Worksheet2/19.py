83774f1f2c8e0561907d1eac5e748f55e3bf9dd7
Rahul0506 <rahul.gidijala@gmail.com>
2017-03-18 21:42:38 +0800

    Add files via upload

--- Worksheet2/19.py
+++ Worksheet2/19.py
@@ -8,6 +8,8 @@ def checkRight(board, row, col):
 		return False
 		
 def checkLeft(board, row, col):
+	if col-1 < 0:
+		return False
 	try:
 		if board[row][col-1]:
 			return False
@@ -17,6 +19,8 @@ def checkLeft(board, row, col):
 		return False
 		
 def checkUp(board, row, col):
+	if row-1 < 0:
+		return False
 	try:
 		if board[row-1][col]:
 			return False
@@ -38,21 +42,22 @@ def display(maze):
 	for i in maze:
 		print(i)
 		
-def move(maze, cur_row, cur_col):
+def move(maze, cur_row, cur_col, end_col_index, end_row_index):
 	if (cur_row == end_row_index) and (cur_col == end_col_index):
 		return True
 	else:
+		maze[cur_row][cur_col] = 1
 		if checkLeft(maze, cur_row, cur_col):
-			return move(maze, cur_row, cur_col - 1)
+			return move(maze, cur_row, cur_col - 1, end_col_index, end_row_index)
 		
 		if checkRight(maze, cur_row, cur_col):
-			return move(maze, cur_row, cur_col + 1)
+			return move(maze, cur_row, cur_col + 1, end_col_index, end_row_index)
 			
 		if checkUp(maze, cur_row, cur_col):
-			return move(maze, cur_row - 1, cur_col)
+			return move(maze, cur_row - 1, cur_col, end_col_index, end_row_index)
 			
 		if checkDown(maze, cur_row, cur_col):
-			return move(maze, cur_row + 1, cur_col)
+			return move(maze, cur_row + 1, cur_col, end_col_index, end_row_index)
 		else:
 			return False
 
@@ -69,18 +74,18 @@ while True:
 	end_col_index = random.randint(0, cols-1)
 	if start_row_index != end_row_index or start_col_index != end_col_index:
 		break
-		
+
 maze = []
 
 for i in range(rows):
 	maze.append([])
 	for j in range(cols):
 		maze[i].append(random.randint(0, 1))
-		
+				
 maze[start_row_index][start_col_index] = 0
 maze[end_row_index][end_col_index] = 0
 
 display(maze)
 print(start_row_index, start_col_index)
 print(end_row_index, end_col_index)
-print(move(maze, start_row_index, start_col_index))
+print(move(maze, start_row_index, start_col_index, end_col_index, end_row_index))
