a1a86c86062a93bda45bb9f51e2ed27ec5fa3662
Rahul0506 <rahul.gidijala@gmail.com>
2017-03-19 22:49:15 +0800

    Add files via upload

--- Worksheet2/19.py
+++ Worksheet2/19.py
@@ -48,44 +48,44 @@ def move(maze, cur_row, cur_col, end_col_index, end_row_index):
 	else:
 		maze[cur_row][cur_col] = 1
 		if checkLeft(maze, cur_row, cur_col):
-			return move(maze, cur_row, cur_col - 1, end_col_index, end_row_index)
+			if not move(maze, cur_row, cur_col - 1, end_col_index, end_row_index):
+				if checkRight(maze, cur_row, cur_col):
+					if not move(maze, cur_row, cur_col + 1, end_col_index, end_row_index):
+						if checkUp(maze, cur_row, cur_col):
+							if not move(maze, cur_row - 1, cur_col, end_col_index, end_row_index):
+								if checkDown(maze, cur_row, cur_col):
+									return move(maze, cur_row + 1, cur_col, end_col_index, end_row_index)
+								else:
+									return False
 		
-		if checkRight(maze, cur_row, cur_col):
-			return move(maze, cur_row, cur_col + 1, end_col_index, end_row_index)
-			
-		if checkUp(maze, cur_row, cur_col):
-			return move(maze, cur_row - 1, cur_col, end_col_index, end_row_index)
-			
-		if checkDown(maze, cur_row, cur_col):
-			return move(maze, cur_row + 1, cur_col, end_col_index, end_row_index)
-		else:
-			return False
-
+		return False
 
 import random
 
 rows = 20
 cols = 20
 
-while True:
-	start_row_index = random.randint(0, rows-1)
-	start_col_index = random.randint(0, cols-1)
-	end_row_index = random.randint(0, rows-1)
-	end_col_index = random.randint(0, cols-1)
-	if start_row_index != end_row_index or start_col_index != end_col_index:
+while True:			#Just to check if the algorithm ever encouters a maze where it finds a path
+	while True:
+		start_row_index = random.randint(0, rows-1)
+		start_col_index = random.randint(0, cols-1)
+		end_row_index = random.randint(0, rows-1)
+		end_col_index = random.randint(0, cols-1)
+		if start_row_index != end_row_index or start_col_index != end_col_index:
+			break
+
+	maze = []
+
+	for i in range(rows):
+		maze.append([])
+		for j in range(cols):
+			maze[i].append(random.randint(0, 1))
+					
+	maze[start_row_index][start_col_index] = 0
+	maze[end_row_index][end_col_index] = 0
+
+	if move(maze, start_row_index, start_col_index, end_col_index, end_row_index):
+		display(maze)
+		print(start_row_index, start_col_index)
+		print(end_row_index, end_col_index)
 		break
-
-maze = []
-
-for i in range(rows):
-	maze.append([])
-	for j in range(cols):
-		maze[i].append(random.randint(0, 1))
-				
-maze[start_row_index][start_col_index] = 0
-maze[end_row_index][end_col_index] = 0
-
-display(maze)
-print(start_row_index, start_col_index)
-print(end_row_index, end_col_index)
-print(move(maze, start_row_index, start_col_index, end_col_index, end_row_index))
